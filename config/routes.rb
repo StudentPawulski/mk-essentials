d228a53b28563af2804af4e917553ba0f8c6ea1a
studentpawulski <spawulski@academic.rrc.ca>
2019-11-03 14:53:52 -0600

    added static about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'static/show'
   get 'order/index'
   get 'status/index'
   get 'category/index'
   get 'product/index'
   get 'product/show'
+
+  get '/:page' => 'static#show'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
