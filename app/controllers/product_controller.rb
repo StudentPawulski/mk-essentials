635e3d129bd1f2594d3a577660ada27e813c2d89
studentpawulski <spawulski@academic.rrc.ca>
2019-11-03 15:55:07 -0600

    Deleted wrong product controller and added Products controller

--- app/controllers/product_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ProductController < ApplicationController
-  def index; end
-
-  def show; end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'products/index'
   get 'static/show'
   get 'order/index'
   get 'status/index'
@@ -12,5 +13,9 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  resources :products, only: :index
+
+  root to: 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
