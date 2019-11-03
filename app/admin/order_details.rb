c2006e73aa67e136b0f7ece116d02454a29f11fe
spawulski@academic.rrc.ca <spawulski@MSI.localdomain>
2019-11-03 11:50:27 -0600

    Added base model

---
+++ app/admin/order_details.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register OrderDetail do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :order, :product, :product_price, :product_qty, :sales_tax, :total_cost
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:order, :product, :product_price, :product_qty, :sales_tax, :total_cost]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
