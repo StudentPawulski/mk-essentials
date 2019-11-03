01048bde9749d7dc9802d09f4c23e39a6743656b
studentpawulski <spawulski@academic.rrc.ca>
2019-11-03 13:06:20 -0600

    Cleaning up db mistakes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_03_190459) do
+ActiveRecord::Schema.define(version: 2019_11_03_190601) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -89,7 +89,6 @@ ActiveRecord::Schema.define(version: 2019_11_03_190459) do
     t.string "name"
     t.float "price"
     t.text "desc"
-    t.string "category"
     t.integer "quantity"
     t.float "discount"
     t.boolean "available"
