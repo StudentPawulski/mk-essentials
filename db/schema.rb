e76c04bb9dfbc47142c3655ccc3f41517564b14f
spawulski@academic.rrc.ca <spawulski@MSI.localdomain>
2019-11-03 11:58:22 -0600

    Added categories to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_03_164618) do
+ActiveRecord::Schema.define(version: 2019_11_03_175403) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -81,6 +81,9 @@ ActiveRecord::Schema.define(version: 2019_11_03_164618) do
     t.boolean "available"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  add_foreign_key "products", "categories"
 end
