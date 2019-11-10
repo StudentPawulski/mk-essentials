1fd8c4d7638e2bb726a3be732c630c65bf4a133f
studentpawulski <spawulski@academic.rrc.ca>
2019-11-10 09:54:33 -0600

    Working on category/show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
-  # def index
-  #  # before_action :authenticate_member!
-  #  @products = Product.all
-  #  # @categories = Product.category_id.all
-  # end
+  def index
+    @products = Product.order(:name)
+    # @categories = Product.category_id.all
+  end
 
   def show
     @product = Product.find(params[:id])
