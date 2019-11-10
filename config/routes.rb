1f9a26a3b71cc6b1bc69da47f45bbb2a066720f5
studentpawulski <spawulski@academic.rrc.ca>
2019-11-10 09:31:46 -0600

    Working on category/show

--- config/routes.rb
+++ config/routes.rb
@@ -27,8 +27,6 @@ Rails.application.routes.draw do
   resources :categories, only: %i[index show]
   resources :pages, only: %i[index show]
 
-  # get '/:page' => 'static#show'
-
   root to: 'categories#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
