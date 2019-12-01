c757b957c8e0eb4e6b002059bad49d853f015e40
studentpawulski <spawulski@academic.rrc.ca>
2019-12-01 16:00:47 -0600

    working on docker

--- db/migrate/20191103163346_create_order_details.rb
+++ db/migrate/20191103163346_create_order_details.rb
@@ -3,12 +3,12 @@
 class CreateOrderDetails < ActiveRecord::Migration[6.0]
   def change
     create_table :order_details do |t|
-      # t.string :order
-      # t.string :product
+      t.string :order
+      t.string :product
       t.float :product_price
       t.integer :product_qty
-      # t.float :sales_tax
-      # t.float :total_cost
+      t.float :sales_tax
+      t.float :total_cost
 
       t.timestamps
     end
